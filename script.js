68ea94dab449abb593919db4c5de84c474e40e31
jabbaum <jabbaum@gmail.com>
2023-12-12 17:33:15 -0800

    made it so that the board size can be determined programmatically via setCellCount and adjusted how the size of the cells was determined to use js to set the size

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function cellCreator () {
     let sketchPad = document.querySelector(`.sketchPad`);
     let cell = null;
-    for (let i = 0; i < 256; i++) {
+    let cellCount = setCellCount();
+    let boardSize = cellCount * cellCount;
+    let cellSize = Math.floor(960/cellCount);
+    console.log(cellSize);
+    for (let i = 0; i < boardSize; i++) {
         
         cell = document.createElement('div');
         cell.setAttribute( `class`, `cell`);
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
         cell.addEventListener('mouseenter', onHover);
         sketchPad.appendChild(cell);
     }
@@ -14,6 +20,11 @@ function cellCreator () {
 
 function onHover() {
     this.setAttribute('class', 'cell once');
+}
+
+function setCellCount() {
+    let cellCount = 16;
+    return cellCount;
 }
 
 addEventListener("load", cellCreator);
